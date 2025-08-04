608a274312ad30ec63a2531016905f2a1f156032
nairtanmay <nairtanmaybiju@email.com>
2025-08-04 22:05:03 +0530

    h9

--- app/scoreboards/kumite/page.jsx
+++ app/scoreboards/kumite/page.jsx
@@ -237,7 +237,7 @@ const togglePenalty = (color, type) => {
     </h2>
 
   </div>
-   <h1 className="text-4xl font-bold mb-4 z-10 glow-text pt-8">
+   <h1 className="text-4xl font-bold mb-4 z-10 glow-text pt-12">
   {winner ? (winner === "draw" ? "Draw!" : `${winner.toUpperCase()} Wins!`) : "Kumite Scoreboard"}
 </h1>
 
