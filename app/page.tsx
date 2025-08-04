4f5204efaba0ad9d30505c66c2e114bbff69de29
nairtanmay <nairtanmaybiju@email.com>
2025-08-04 18:33:49 +0530

    h5

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,8 @@ export default function Dashboard() {
       </div>
 
       {/* Main Dashboard Content */}
-      <main className="flex flex-col items-center justify-center text-white relative z-10 py-20 pt-32">
+    <main className="flex flex-col items-center justify-center text-white relative z-10 py-10 md:py-16 lg:py-20">
+
         <h1 className="text-5xl font-extrabold mb-16 drop-shadow-lg">Dashboard</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-12 w-full max-w-6xl px-4">
